6cd23f09c0cb756310be74c2ae11637a35f56a4c
iosebi2899 <45330280+iosebi2899@users.noreply.github.com>
2021-09-10 17:30:11 +0400

    test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import './App.css';
+import React from 'react'
+import './App.scss'
+import Header from './components/Header'
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>ველოდებით ელენეს</h1> 
-    </div>
+    <>
+      <div  className="header">
+         <Header />
+      </div>
+    </>
   );
 }
 
 export default App;
+
